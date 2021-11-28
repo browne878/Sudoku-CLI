5acd60a5d28a1dc6e5d159c662be0f848d9b2d5b
browne878 <78582151+browne878@users.noreply.github.com>
2021-11-28 02:28:20 +0000

    Add logic for edit_board method

--- classes/board.py
+++ classes/board.py
@@ -1,4 +1,42 @@
+import copy
+
 class Board:
 
     def __init__(self):
-        print('Welcome!')
+        self.board = ["¦===================================¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦===========¦===========¦===========¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦===========¦===========¦===========¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦ _ | _ | _ ¦ _ | _ | _ ¦ _ | _ | _ ¦",
+                      "¦===================================¦"]
+        self.column_index = [2, 6, 10, 14, 18, 22, 26, 30, 34]
+    
+    def edit_board(self, row, column, num):
+        temp_board = copy.copy(self.board)
+        del temp_board[0]
+        del temp_board[3]
+        del temp_board[6]
+        del temp_board[-1]
+
+        index = self.board.index(temp_board[row - 1])
+
+        temp_board[row - 1] = temp_board[row - 1][:self.column_index[column - 1]] + num + temp_board[row - 1][self.column_index[column - 1] + 1:]
+
+        for row in temp_board:
+            print(row)
+        
+        print('')
+
+        self.board[index] = temp_board[row - 1]
+
+        print(self.board)
+        
+        # for row in self.board:
+        #     print(row)
