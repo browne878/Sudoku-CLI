a5e37f15f89fdb320ea9f25b3b3405129ff0b2a7
browne878 <78582151+browne878@users.noreply.github.com>
2021-12-12 18:45:08 +0000

    Add space between header and board

--- classes/game.py
+++ classes/game.py
@@ -163,8 +163,9 @@ class Game:
             '========================= SUDOKU CLI =========================',
             f'Correct Guesses = {correct_guesses}                       ' +
             f'Incorrect Guesses = {incorrect_guesses}',
-            f'                       Total Guesses = '
-            f'{correct_guesses + incorrect_guesses}'
+            f'                       Total Guesses = ' +
+            f'{correct_guesses + incorrect_guesses}',
+            ''
         ]
 
         for line in self.game_header:
